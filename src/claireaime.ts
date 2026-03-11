c88671de5ef5bb2c611d295baacf4ebfb128279b
Olivier Poppon <olivier.poppon@tempo-one.com>
2026-03-11 15:34:03 +0100

    init

---
+++ src/claireaime.ts
@@ -0,0 +1,8 @@
+const author = {
+  name: "Claire-Aime",
+  instagram: "https://www.instagram.com/classeclaireaime21/",
+  linkedin: "https://www.linkedin.com/in/classe-claire-aime",
+  email: "mailto:classeclaireaime@gmail.com",
+};
+
+export { author };
