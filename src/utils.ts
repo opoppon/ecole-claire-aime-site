9c7c0b7195b8796fd6997db15e6774f3b8759b97
Olivier Poppon <olivier.poppon@tempo-one.com>
2026-03-17 09:14:14 +0100

    tri des posts et formattage date

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function formatDate(date: string | Date) {
+  return new Intl.DateTimeFormat('fr-FR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date(date));
+}
