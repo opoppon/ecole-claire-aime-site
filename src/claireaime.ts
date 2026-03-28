c380942a3078412c6312918ccb602f6deb5aebe5
Olivier Poppon <olivier.poppon@tempo-one.com>
2026-03-28 14:25:06 +0100

    logo dans header

--- src/claireaime.ts
+++ src/claireaime.ts
@@ -1,5 +1,5 @@
 const author = {
-  name: "Classe Claire-Aime",
+  name: "Ecole Claire-Aime",
   instagram: "https://www.instagram.com/classeclaireaime21/",
   linkedin: "https://www.linkedin.com/in/classe-claire-aime",
   email: "mailto:classeclaireaime@gmail.com",
